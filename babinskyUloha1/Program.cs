89502c50f36dd3d67b1824d57e67e5c094323a00
Matúš Babinský <matusbabinsky@yahoo.com>
2019-10-05 12:11:23 +0200

    skoroHotova

--- babinskyUloha1/Program.cs
+++ babinskyUloha1/Program.cs
@@ -14,6 +14,7 @@ namespace babinskyUloha1
             Console.WriteLine(input);
             var separators = new string[] { ",", ".", "!", "\'", " ", "\'s" };
             var words = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            
             //int numOfCharsWithoutSpaces = Regex.Replace(input, " ", String.Empty).Length;
             //foreach (string word in input.Split(separators, StringSplitOptions.RemoveEmptyEntries)) Console.WriteLine(word);
 
@@ -23,13 +24,45 @@ namespace babinskyUloha1
             Console.WriteLine("Number of characters (no spaces): " + Regex.Replace(input, " ", string.Empty).Length);
             
             Console.WriteLine("Number of vowels: " + GetNumberOfVowels(input));
-            Console.WriteLine("Number of consonants: " + (Regex.Replace(input, "[^A-Za-z0-9]", string.Empty).Length - GetNumberOfVowels(input)));
+            Console.WriteLine("Number of consonants: " + ( GetNumberOfConstants(Regex.Replace(input, "[^A-Za-z0-9]", string.Empty))));
             
             Console.WriteLine("Number of words: " + words.Length);
-            //Console.WriteLine("Number of unique words: " + WordsToDictionary(words));
-            Console.WriteLine("Number of sentences: " + NumberOfSentences(input));
+            Console.WriteLine("Number of unique words: " + WordsToDictionary(words).Count);
+            var numberOfSentences = NumberOfSentences(input);
+            Console.WriteLine("Number of sentences: " + numberOfSentences);
+            Console.WriteLine("Average sentence length (words): " +((double)words.Length/numberOfSentences) );
             //Word word = new Word(input);
             //Console.WriteLine("Hello World!");
+        }
+
+        private static int GetNumberOfConstants(string input)
+        {
+            bool reducer = false;
+            int counter = 0;
+            char[] vowels = { 'i', 'e', 'a', 'o', 'u', 'y' };
+            foreach (var ch in input)
+            {
+                if (!vowels.Contains(ch))
+                {
+
+                    counter++;
+                    if (!ch.Equals('c'))
+                    {
+                        if (ch.Equals('h')&&reducer)
+                        {
+                            counter--;
+                        }
+                        reducer = false;
+                    }
+                    else
+                    {
+                        reducer = true;
+                    }
+                }
+            }
+            //input.Count(x => vowels.Contains(x)); ;
+
+            return counter;
         }
 
         private static int NumberOfSentences(string input)
@@ -72,8 +105,35 @@ namespace babinskyUloha1
         private static int GetNumberOfVowels(in string input)
         {
             //Toto je jednoducha prva veta. 10,14  Toto je druha veta. 7,8 Tretia veta. 5,5 Poslednou bude veta, ktora je stvrta. 12,18
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y'};
-            return input.Count(x => vowels.Contains(x)); ;
+            char[] vowels = { 'i', 'e','a', 'o', 'u', 'y'};
+            char[] vowelsDual = { 'e', 'a',  'u' };
+            int counter =0;
+            bool reducer = false;
+            foreach (var ch in input)
+            {
+                if (vowels.Contains(ch))
+                {
+                    if (ch=='i')
+                    {
+                        reducer = true;
+                        counter++;
+
+                    }
+                    else if (!vowelsDual.Contains(ch)||!reducer)
+                    {
+                        counter++;
+                        reducer = false;
+                    }
+                    else
+                    {
+                        reducer = false;
+                        //Console.WriteLine("i"+ch);
+                    }
+                    
+                }
+            }
+            
+            return counter; 
         }
         
 
@@ -96,11 +156,16 @@ namespace babinskyUloha1
             var things = from character in dict
                 orderby character.Value descending
                 select character;
-            foreach (var word in things)
+
+
+            /*foreach (var word in things)
             {
                 Console.WriteLine (" "+word);
-            }
+            }*/
+            
             return dict;
         }
+
+        
     }
 }
